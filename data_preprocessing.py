cc4c738a70cf606ff40d9982c01508ff96deafe4
Jakub Zeifert <kuba1102@gmail.com>
2023-12-07 19:07:59 +0000

    Remove plot_remote_jobs_by_state function

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -10,25 +10,3 @@ def convert_to_yearly(sal, pay_period):
         return sal * 52
     else:
         return sal
-
-# Plots the distribution of salaries by state with a treshold (optional)
-def plot_remote_jobs_by_state(jobs_df, treshold=None):
-    counts = jobs_df.groupby(['st_code', 'is_remote']).size().unstack()
-    counts['total'] = counts.sum(axis=1)
-
-    if treshold is not None and isinstance(treshold, int):
-        filtered_counts = counts[counts['total'] < treshold].copy()
-    else:
-        filtered_counts = counts.copy()
-
-    filtered_counts.sort_values(by='total', ascending=False, inplace=True)
-
-    del filtered_counts['total']
-    filtered_counts.plot(kind='bar', stacked=True, figsize=(15, 10))
-
-    plt.xlabel('State Code')
-    plt.ylabel('Number of Jobs')
-    plt.title('Number of Remote and Non-Remote Jobs by State')
-
-    plt.legend(['Non-Remote', 'Remote'], loc='upper right')
-    plt.show()
