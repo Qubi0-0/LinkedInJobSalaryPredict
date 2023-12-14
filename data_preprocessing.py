b0d9f9e3574293b577d22a86ce285cecc2a76dde
Jakub Zeifert <kuba1102@gmail.com>
2023-12-14 19:54:30 +0000

    Refactor data preprocessing and EDA code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,27 +13,3 @@ def convert_to_yearly(sal, pay_period):
     else:
         return sal
     
-# Encode job titles
-def encode_job_ttls(jobs_df, print_stats=False):
-    mean_salary = jobs_df.groupby('xp_lvl')['med_sal'].mean()
-    mean_salary_sorted = mean_salary.sort_values(ascending=False)
-    if print_stats:
-        most_significant_job = mean_salary_sorted.index[0]
-        print('Mean salary by experience level:')
-        print(mean_salary_sorted)
-        print(most_significant_job)
-        print('\n')
-    jobs_df = jobs_df.sort_values(by='med_sal')
-
-    le = LabelEncoder()
-    jobs_df['Job_Ttl'] = le.fit_transform(jobs_df['Job_Ttl'])
-    
-    return jobs_df
-
-# Normalize data
-def normalize_data(jobs_df):
-    scaler = MinMaxScaler()
-    jobs_df['med_sal'] = scaler.fit_transform(jobs_df['med_sal'].values.reshape(-1, 1))
-    jobs_df['exp_lvl'] = scaler.fit_transform(jobs_df['exp_lvl'].values.reshape(-1, 1))
-    return jobs_df
-
